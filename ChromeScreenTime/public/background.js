fa05106658f64a25467f723f919c9721f0d5c476
Namkhang Le <Namkhangnle@hotmail.com>
2024-09-28 12:47:53 -0400

    Add hostname tracking to background.js

--- ChromeScreenTime/public/background.js
+++ ChromeScreenTime/public/background.js
@@ -1,6 +1,25 @@
 chrome.tabs.onActivated.addListener((tabId, tab) => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        let url = tabs[0].url;
-        console.log(url);
+        if (tabs.length > 0) {
+            let url = tabs[0].url;
+            if (url) {
+                try {
+                    let hostname = new URL(url).hostname;
+                    chrome.storage.local.get(['hostnames'], (result) => {
+                        let hostnames = result.hostnames || {};
+                        if (hostnames[hostname]) {
+                            hostnames[hostname]++;
+                        } else {
+                            hostnames[hostname] = 1;
+                        }
+                        chrome.storage.local.set({ hostnames }, () => {
+                            console.log('Hostname count updated:', hostnames);
+                        });
+                    });
+                } catch (e) {
+                    console.error('Invalid URL:', url);
+                }
+            }
+        }
     });
 })
