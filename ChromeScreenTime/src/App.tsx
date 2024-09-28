45ae376310362fc01589ffc01ba5fc5cdb724a3c
Namkhang Le <Namkhangnle@hotmail.com>
2024-09-28 15:19:43 -0400

    Refactor package.json and App.tsx to add chart.js and react-chartjs-2 dependencies

--- ChromeScreenTime/src/App.tsx
+++ ChromeScreenTime/src/App.tsx
@@ -1,7 +1,9 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useState, useEffect } from 'react'
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 function App() {
   const [urls, setUrls] = useState<{ [key: string]: number }>({});
@@ -17,22 +19,44 @@ function App() {
     chrome.scripting.executeScript({
       target: {tabId: tab.id!},
       func: () => {
-        alert('Hello from the ');
+        alert('Hello from the interside');
       }
     });
   }
 
+  const data = {
+    labels: Object.keys(urls),
+    datasets: [
+      {
+        label: 'Time Spent (ms)',
+        data: Object.values(urls),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <h1>Chrome Screen Time</h1>
+      <div className = "chart-container">
+        <Doughnut data={data} />
       </div>
-      <h1>Chrome Screen Time</h1>
       <div className="card">
         <button onClick={() => onclick()}>
           Click me
